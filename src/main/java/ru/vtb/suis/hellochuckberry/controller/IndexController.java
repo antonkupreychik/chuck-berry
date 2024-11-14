fcb8baee1eb46065b8556ec2a440adac4d19cc3d
Купрейчик Антон Леонидович (4097740) <kupreichik@dev.vtb.ru>
2024-11-14 19:43:42 +0300

    ANT-2: update java version

--- src/main/java/ru/vtb/suis/hellochuckberry/controller/IndexController.java
+++ src/main/java/ru/vtb/suis/hellochuckberry/controller/IndexController.java
@@ -1,13 +1,15 @@
 package ru.vtb.suis.hellochuckberry.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class IndexController {
 
 
-    @GetMapping("/index")
+    @RequestMapping("/")
+    @ResponseBody
     public String index() {
         return "Hello World!";
     }

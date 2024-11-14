129418b1d6296c44788402f730d714b745edbce2
Купрейчик Антон Леонидович (4097740) <kupreichik@dev.vtb.ru>
2024-11-14 19:02:00 +0300

    first commit

---
+++ src/main/java/ru/vtb/suis/hellochuckberry/HelloChuckBerryApplication.java
@@ -0,0 +1,13 @@
+package ru.vtb.suis.hellochuckberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloChuckBerryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloChuckBerryApplication.class, args);
+    }
+
+}
